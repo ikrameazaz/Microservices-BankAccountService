72c12d04f49cdb2d099170974b3459a33beadb92
ikrame <ikrameazaz@gmail.com>
2025-10-30 21:10:33 +0100

    GraphQl Controller

--- bank-account-service/src/main/java/org/sid/bank_account_service/web/BankAccountGraphQLController.java
+++ bank-account-service/src/main/java/org/sid/bank_account_service/web/BankAccountGraphQLController.java
@@ -3,6 +3,7 @@ package org.sid.bank_account_service.web;
 import org.sid.bank_account_service.entities.BankAccount;
 import org.sid.bank_account_service.repositories.BankAccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.graphql.data.method.annotation.Argument;
 import org.springframework.graphql.data.method.annotation.QueryMapping;
 import org.springframework.stereotype.Controller;
 import java.util.List;
@@ -15,6 +16,12 @@ public class BankAccountGraphQLController {
     @QueryMapping
     public List<BankAccount> accountsList(){
         return bankAccountRepository.findAll();
+    }
+
+    @QueryMapping
+    public BankAccount bankAccountById(@Argument String id){
+        return bankAccountRepository.findById(id).orElseThrow(()-> new RuntimeException(String.format("Bank account with id %s not found", id)));
+
     }
 
 
